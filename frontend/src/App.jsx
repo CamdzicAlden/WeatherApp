7123753758ad7028e1064179fc69e9aaddbbd684
CamdzicAlden <aldencamdzic3@gmail.com>
2026-02-13 22:12:25 +0100

    feat: Add background rectangles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import BackgroundCircle from "./components/BackgroundCircle";
+import BackgroundRectangle from "./components/BackgroundRectangle";
 
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
       <BackgroundCircle top={35} left={30} />
       <BackgroundCircle top={65} left={65} />
       <BackgroundCircle top={100} left={100} />
+
+      <BackgroundRectangle top={90} left={5} />
+      <BackgroundRectangle top={108} left={6} />
+      <BackgroundRectangle top={80} left={-4} />
+
+      <BackgroundRectangle top={5} left={95} />
+      <BackgroundRectangle top={12} left={104} />
+      <BackgroundRectangle top={-12} left={94} />
     </div>
   );
 }
---
+++ frontend/src/components/BackgroundRectangle.jsx
@@ -0,0 +1,10 @@
+function BackgroundRectangle({ top, left }) {
+  return (
+    <div
+      className="fixed w-[30%] h-[15%] bg-[#0D47A1] rounded-[999999px] rotate-[-35deg] shadow-[-0.12rem_0.12rem_0_rgba(0,0,0,0.05),0.12rem_0.12rem_0_rgba(0,0,0,0.05)] blur-[0.10dvw] -translate-x-1/2 -translate-y-1/2"
+      style={{ top: `${top}%`, left: `${left}%` }}
+    />
+  );
+}
+
+export default BackgroundRectangle;
