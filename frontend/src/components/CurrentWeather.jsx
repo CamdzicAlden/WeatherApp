754cbc3fb562a4e63e8f936e1b1b8205a4ec6dc8
CamdzicAlden <aldencamdzic3@gmail.com>
2026-02-16 07:47:12 +0100

    fix: Adjust spacing between items in CurrentWeather

--- frontend/src/components/CurrentWeather.jsx
+++ frontend/src/components/CurrentWeather.jsx
@@ -4,12 +4,16 @@ import CityTag from "./CityTag.jsx";
 function CurrentWeather({ city, image, weatherText, currentTemp, maxMinTemp }) {
   return (
     //Main flex container
-    <div className="flex flex-col justify-evenly items-center text-[#fafafa]">
+    <div className="flex flex-col justify-evenly items-center gap-4 text-[#fafafa] mt-[2%]">
       {/* Location tag */}
       <CityTag city={city} />
 
       {/* Current weather image */}
-      <img src={image} alt="I" />
+      <img
+        src={image}
+        alt="I"
+        className="w-[clamp(0.1rem,15dvw,20rem)] h-auto mt-[-3%] mb-[-5%] ml-[-2%]"
+      />
 
       {/* Paragraphs with weather information */}
       <p
