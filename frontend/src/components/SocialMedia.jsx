04d9e6a365dc5d92606e9b2b370e4c75b779d3d2
CamdzicAlden <aldencamdzic3@gmail.com>
2026-02-21 12:10:19 +0100

    feat: Make social media icons be clickable and refer to my social media profiles

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,28 +1,40 @@
 //SocialMedia component for displaying social media icons
 function SocialMedia() {
+  const socials = [
+    {
+      href: "https://www.linkedin.com/in/aldencamdzic/",
+      icon: "/icons/LinkedIn.svg",
+      name: "LinkedIn",
+    },
+    {
+      href: "https://www.instagram.com/_camdzicc_?igsh=MTVjanY3M3BrMm5lMg==",
+      icon: "/icons/Instagram.svg",
+      name: "Instagram",
+    },
+    {
+      href: "https://github.com/CamdzicAlden",
+      icon: "/icons/GitHub.svg",
+      name: "GitHub",
+    },
+  ];
+
   return (
     //Main flex container
     <div className="flex justify-center items-center gap-2">
-      {/* LinkedIn icon */}
-      <img
-        src="/icons/LinkedIn.svg"
-        alt="I"
-        className="w-[clamp(0.1rem,1.5dvw,10rem)] h-auto"
-      />
-
-      {/* Instagram icon */}
-      <img
-        src="/icons/Instagram.svg"
-        alt="I"
-        className="w-[clamp(0.1rem,1.5dvw,10rem)] h-auto"
-      />
-
-      {/* GitHub icon */}
-      <img
-        src="/icons/GitHub.svg"
-        alt="I"
-        className="w-[clamp(0.1rem,1.5dvw,10rem)] h-auto"
-      />
+      {socials.map((social) => (
+        <a
+          key={social.name}
+          href={social.href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={social.icon}
+            alt=""
+            className="w-[clamp(0.1rem,1.5dvw,10rem)] h-auto"
+          />
+        </a>
+      ))}
     </div>
   );
 }
