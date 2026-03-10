de2d88e40ac655242a1f5376fb403a7d21f7fa16
CamdzicAlden <aldencamdzic3@gmail.com>
2026-03-10 08:52:51 +0100

    fix: Change allowed origin in backend server

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,22 @@ const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT || 5000;
 const BASE_URL = "https://api.weatherapi.com/v1";
 
+//Map for holding cached data on server
 const cache = new Map();
 
 //Making http server
 const server = http.createServer(async (req, res) => {
-  //Allow access to data from all origins to prevent CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  //Allow only this website to access api
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://weather-app-psi-five-97.vercel.app",
+  );
+  //Only allow those http methods
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  //Allow Content-Type in header
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle preflight OPTIONS requests
+  // Handle preflight (test request)
   if (req.method === "OPTIONS") {
     res.writeHead(200);
     res.end();
@@ -29,9 +35,9 @@ const server = http.createServer(async (req, res) => {
   //Extracting pathname and query from url
   const { pathname, searchParams } = url;
 
-  //If pathname is /currentWeather
+  //If pathname is /weatherData
   if (pathname === "/weatherData") {
-    //Get lat and lon from query
+    //Get lat, lon and dt from query
     const lat = searchParams.get("lat");
     const lon = searchParams.get("lon");
     const dt = searchParams.get("dt");
@@ -43,8 +49,10 @@ const server = http.createServer(async (req, res) => {
       return;
     }
 
+    //Key for storing data in cache
     const key = `${lat},${lon},${dt}`;
 
+    //If cache already contains key return data instantly
     if (cache.has(key)) {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(cache.get(key)));
@@ -52,24 +60,30 @@ const server = http.createServer(async (req, res) => {
     }
 
     try {
+      //Destructuring result from Promise.all
       const [current, forecast, astronomy, history] = await Promise.all([
+        //Fetching current.json
         fetch(
           `${BASE_URL}/current.json?key=${API_KEY}&q=${lat},${lon}&aqi=yes`,
         ).then((r) => r.json()),
 
+        //Fetching forecast.json
         fetch(
           `${BASE_URL}/forecast.json?key=${API_KEY}&q=${lat},${lon}&days=2`,
         ).then((r) => r.json()),
 
+        //Fetching astronomy.json
         fetch(`${BASE_URL}/astronomy.json?key=${API_KEY}&q=${lat},${lon}`).then(
           (r) => r.json(),
         ),
 
+        //Fetching history.json
         fetch(
           `${BASE_URL}/history.json?key=${API_KEY}&q=${lat},${lon}&dt=${dt}`,
         ).then((r) => r.json()),
       ]);
 
+      //Making result object from destructured data
       const result = {
         current: current.current,
         cityName: current.location.name,
@@ -78,9 +92,12 @@ const server = http.createServer(async (req, res) => {
         history: history.forecast.forecastday,
       };
 
+      //Store key and result data in cache
       cache.set(key, result);
+      //Start a 60s timeout that will delete cache after
       setTimeout(() => cache.delete(key), 60000);
 
+      //Returning result as string
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(result));
     } catch (err) {
@@ -90,24 +107,27 @@ const server = http.createServer(async (req, res) => {
 
     //If pathname is /searchCity
   } else if (pathname === "/searchCity") {
-    //Get lat and lon from query
+    //Get city from query
     const city = searchParams.get("city");
 
     //Validate query parameters
     if (!city) {
       res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "Missing lat or lon" }));
+      res.end(JSON.stringify({ error: "Missing city" }));
       return;
     }
 
+    //Fetching search.json
     const result = await fetch(
       `${BASE_URL}/search.json?key=${API_KEY}&q=${city}`,
     );
     const data = await result.json();
 
-    //Write header and return json as a string
+    //Write header and return data as string
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data));
+
+    //If no endpoint found
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Endpoint not found" }));
